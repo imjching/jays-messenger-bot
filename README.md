0dd3dbcb1f251ea581c4d065881e0300b65a945f
Jay <imjching@users.noreply.github.com>
2017-05-15 11:30:16 -0700

    cleanup

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This listener allows you to execute bash commands through the bot.
 Do remember to set `process.env.ACCESS_TOKEN` to your application's Access Token.
 
 To see full instructions on how to enable in Facebook. [Follow the official guide](https://developers.facebook.com/docs/messenger-platform/quickstart)
+
+![messengerbot.png](messengerbot.png)
--- index.js
+++ index.js
@@ -26,59 +26,9 @@ function sendTextMessage(sender, text) {
   });
 }
 
-function sendGenericMessage(sender) {
-  messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "First card",
-          "subtitle": "Element #1 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "Web url"
-          }, {
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for first element in a generic bubble",
-          }],
-        },{
-          "title": "Second card",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for second element in a generic bubble",
-          }],
-        }]
-      }
-    }
-  };
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
-
 app.use(bodyParser.json());
-
 app.set('port', (process.env.PORT || 8000));
-app.set('verify_token', (process.env.VERIFY_TOKEN || 'f334b099'));
+app.set('verify_token', (process.env.VERIFY_TOKEN || 'g334b099'));
 
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === app.get('verify_token')) {
@@ -94,59 +44,33 @@ var exec = function(cmd, cb){
   var parts = cmd.split(/\s+(.+)?/)[1];
   var ls = child_process.exec(parts, function (err, stdout, stderr) {
     if (err) {
-      console.log(error.stack);
-      console.log('Error code: ' + error.code);
-      console.log('Signal received: ' + error.signal);
-      cb('command "' + cmd + '" exited with wrong status code "' + error.code + '"');
+      console.log(err.stack);
+      console.log('Error code: ' + err.code);
+      console.log('Signal received: ' + err.signal);
+      cb('command "' + cmd + '" exited with wrong status code "' + err.code + '"');
     } else {
       console.log('Child Process STDOUT: '+stdout);
       cb(stdout);
     }
   });
-
   ls.on('exit', function (code) {
     console.log('Child process exited with exit code '+code);
   });
 };
 
 var hello = function(text, callback) {
-  var output = ['List of commands: ', 'Hello', 'startServer', 'memoryUsage', 'prepareDemo'];
-
+  var output = ['List of commands: ', 'Hello', '/exec <command>'];
   callback(output.join('\n'));
 };
 
-var start_server = function (text, callback) {
-  exec('/exec ls -1', callback);
-};
-
-var memory_usage = function(text, callback) {
-
-};
-
-var preparing_demo = function(text, callback) {
-
-};
-
 var exec_command = function(text, callback) {
-  console.log('exec...'+ text);
   exec(text, callback);
 };
 
 var routes = {
   '^Hello': hello,
-  '^startServer': start_server,
-  '^memoryUsage': memory_usage,
-  '^prepareDemo': preparing_demo,
   '^\\/exec\\s[^$]+': exec_command
 };
-
-// def memory_usage(message, match):
-//     output = memReport()
-//     return TextMessageProtocolEntity('\n'.join(output), to=message.getFrom())
-
-// def preparing_demo(message, match):
-//     return TextMessageProtocolEntity('Something awesome is cooking', to=message.getFrom())
-
 
 app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
@@ -160,20 +84,26 @@ app.post('/webhook/', function (req, res) {
     }
     if (event.message && event.message.text) {
       text = event.message.text;
-      console.log(text);
-      if (text === 'Generic') {
-        sendGenericMessage(sender);
-        continue;
-      }
 
       for (var route in routes) {
         if (routes.hasOwnProperty(route)) {
-          console.log('text' + text);
-          console.log('route' + route);
           if (text.match(route)) {
             console.log(routes[route]);
             routes[route](text, function(data) {
-              if (data) {
+              if (data && data.length > 320) {
+                var sum = [];
+                sum.push(data.substring(0, 320));
+                data = data.substring(321);
+
+                while (data.length > 320) {
+                  sum.push(data.substring(0, 320));
+                  data = data.substring(321);
+                }
+
+                for (var i = 0; i < sum.length; i++) {
+                  sendTextMessage(sender, sum[i], token);
+                }
+              } else if (data) {
                 sendTextMessage(sender, data.substring(0, 320), token);
               } else {
                 sendTextMessage(sender, 'No response', token);
